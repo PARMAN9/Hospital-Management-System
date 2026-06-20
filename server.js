438649c6534db0603a583ffe5cca6385e81e85e8
Arman Parwani <armsh0810@gmail.com>
2026-06-20 09:36:47 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,32 +1,85 @@
 const express = require("express");
+const path = require("path");
 
 const app = express();
 
 app.use(express.urlencoded({ extended: true }));
+app.use(express.static(__dirname));
 
 let patients = [];
 
+
 app.get("/", (req, res) => {
-    res.sendFile(__dirname + "/index.html");
+    res.sendFile(path.join(__dirname, "index.html"));
 });
 
 app.post("/register", (req, res) => {
 
-    let patient = {
+    const patient = {
         name: req.body.name,
-        age: req.body.age
+        age: req.body.age,
+        date: req.body.date
     };
 
     patients.push(patient);
 
-    res.send(
-        "<h2>Patient Registered</h2>" +
-        "<p>Name: " + patient.name + "</p>" +
-        "<p>Age: " + patient.age + "</p>" +
-        "<a href='/'>Go Back</a>"
-    );
+    res.redirect("/patients");
+});
+
+
+app.get("/patients", (req, res) => {
+
+    let html = `
+    <!DOCTYPE html>
+    <html>
+
+    <head>
+        <title>Registered Patients</title>
+        <link rel="stylesheet" href="/style.css">
+    </head>
+
+    <body>
+
+    <div class="container">
+
+        <h1>Registered Patients</h1>
+
+        <table>
+            <tr>
+                <th>Name</th>
+                <th>Age</th>
+                <th>Appointment Date</th>
+            </tr>
+    `;
+
+    patients.forEach(patient => {
+        html += `
+        <tr>
+            <td>${patient.name}</td>
+            <td>${patient.age}</td>
+            <td>${patient.date}</td>
+        </tr>
+        `;
+    });
+
+    html += `
+        </table>
+
+        <br>
+
+        <a href="/">
+            <button>Back to Home</button>
+        </a>
+
+    </div>
+
+    </body>
+    </html>
+    `;
+
+    res.send(html);
 });
 
 app.listen(3000, () => {
-    console.log("Server running on port 3000");
+    console.log("Server running at http://localhost:3000");
 });
